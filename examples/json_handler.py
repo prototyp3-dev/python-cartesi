daf7daebdd17391af34b94d52fac37b6c862c2a9
Felipe Fink Grael <felipe@prototyp3.dev>
2023-08-21 14:15:39 -0300

    feat: Example Dapp for JSON Router

---
+++ examples/json_handler.py
@@ -0,0 +1,66 @@
+import json
+import logging
+
+from cartesi import DApp, Rollup, RollupData, JSONRouter
+
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+dapp = DApp()
+json_router = JSONRouter()
+dapp.add_router(json_router)
+
+# This dapp will read and write from this global state dict
+STATE = {}
+
+
+def str2hex(str):
+    """Encodes a string as a hex string"""
+    return "0x" + str.encode("utf-8").hex()
+
+
+def to_jsonhex(data):
+    """Encode as a JSON hex"""
+    return str2hex(json.dumps(data))
+
+
+@json_router.advance({"op": "set"})
+def handle_advance_set(rollup: Rollup, data: RollupData):
+    data = data.json_payload()
+    key = data['key']
+    value = data['value']
+
+    STATE[key] = value
+
+    rollup.report(to_jsonhex({'key': key, 'value': value}))
+    return True
+
+
+@json_router.advance({"op": "get"})
+def handle_advance_get(rollup: Rollup, data: RollupData):
+    data = data.json_payload()
+    key = data['key']
+
+    if key in STATE:
+        rollup.notice(to_jsonhex({'key': key, 'value': STATE[key]}))
+    else:
+        rollup.report(to_jsonhex({'key': key, 'error': 'not found'}))
+
+    return True
+
+
+@json_router.inspect({"op": "get"})
+def handle_inspect_get(rollup: Rollup, data: RollupData):
+    data = data.json_payload()
+    key = data['key']
+
+    if key in STATE:
+        rollup.report(to_jsonhex({'key': key, 'value': STATE[key]}))
+    else:
+        rollup.report(to_jsonhex({'key': key, 'error': 'not found'}))
+
+    return True
+
+
+if __name__ == '__main__':
+    dapp.run()
---
+++ tests/test_json_handler_dapp.py
@@ -0,0 +1,46 @@
+import pytest
+
+from cartesi.test import TestClient
+
+import examples.json_handler
+from examples.json_handler import to_jsonhex
+
+
+@pytest.fixture
+def dapp_client() -> TestClient:
+    client = TestClient(examples.json_handler.dapp)
+    return client
+
+
+def test_simple_set_get(dapp_client: TestClient):
+
+    set_payload = to_jsonhex(
+        {'op': 'set', 'key': 'key_1', 'value': 'value_1'}
+    )
+    dapp_client.send_advance(hex_payload=set_payload)
+
+    assert dapp_client.rollup.status
+    assert len(dapp_client.rollup.notices) == 0
+
+    get_payload = to_jsonhex(
+        {'op': 'get', 'key': 'key_1'}
+    )
+    dapp_client.send_advance(hex_payload=get_payload)
+    assert dapp_client.rollup.status
+    expected_payload = to_jsonhex(
+        {'key': 'key_1', 'value': 'value_1'}
+    )
+    assert dapp_client.rollup.notices[-1]['data']['payload'] == expected_payload
+
+
+def test_get_inexistent(dapp_client: TestClient):
+
+    get_payload = to_jsonhex(
+        {'op': 'get', 'key': 'key_1000'}
+    )
+    dapp_client.send_advance(hex_payload=get_payload)
+    assert dapp_client.rollup.status
+    expected_payload = to_jsonhex(
+        {'key': 'key_1', 'value': 'value_1'}
+    )
+    assert dapp_client.rollup.notices[-1]['data']['payload'] == expected_payload
