bd00f7c5bc58517d7e3858b58fc81e01f11e9489
Felipe Fink Grael <felipe@prototyp3.dev>
2023-11-03 08:55:11 -0300

    DApp Address router

--- cartesi/router/__init__.py
+++ cartesi/router/__init__.py
@@ -3,3 +3,4 @@ from .json import JSONRouter # noqa
 from .url import URLRouter, URLParameters # noqa
 from .abi import ABIRouter # noqa
 from .multi import MultiRouter # noqa
+from .dapp_address import DAppAddressRouter # noqa
