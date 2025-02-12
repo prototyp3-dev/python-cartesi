4227780ce34ce1a55126de03400d5a4ca9e1929e
Felipe Fink Grael <felipe@prototyp3.dev>
2025-02-12 00:20:15 -0300

    Add support for nested models in ABI encoding and decoding

--- cartesi/abi.py
+++ cartesi/abi.py
@@ -1,7 +1,8 @@
 """
 ABI Types and Helpers for Router and Codecs
 """
-from typing import Annotated, get_type_hints, TypeVar
+from inspect import isclass
+from typing import Annotated, get_type_hints, TypeVar, get_args, get_origin
 from dataclasses import dataclass
 
 import eth_abi
@@ -56,14 +57,36 @@ DEFAULT_ABU_TYPES = {
 }
 
 
-def _get_abi_for_type(field_type):
-    """Returns an ABIT
+def _get_abi_for_type(field, field_type) -> str:
+    """Returns an ABIType for a given type.
 
     Parameters
     ----------
-    field_type : _type_
-        _description_
+    field_type : type
+        Type to get the ABIType for
+
+    Returns
+    -------
+    str
+        ABIType name
     """
+    # Get the field metadata
+    metadata = getattr(field_type, '__metadata__', None)
+    if metadata is None:
+        if field_type not in DEFAULT_ABU_TYPES:
+            raise ValueError(f'Field {field} has no ABIType metadata.')
+        metadata = DEFAULT_ABU_TYPES[field_type].__metadata__
+
+    # Get ABIType from metadata
+    abi_type = None
+    for md in metadata:
+        if isinstance(md, ABIType):
+            abi_type = md
+            break
+    if abi_type is None:
+        raise ValueError(f'Field {field} has no ABIType metadata.')
+
+    return abi_type.name
 
 
 def get_abi_types_from_model(model: pydantic.BaseModel) -> list[str]:
@@ -86,25 +109,77 @@ def get_abi_types_from_model(model: pydantic.BaseModel) -> list[str]:
     for field in fields:
         field_type = hints[field]
 
-        # Get the field metadata
-        metadata = getattr(field_type, '__metadata__', None)
-        if metadata is None:
-            if field_type not in DEFAULT_ABU_TYPES:
-                raise ValueError(f'Field {field} has no ABIType metadata.')
-            metadata = DEFAULT_ABU_TYPES[field_type].__metadata__
+        base_type = get_origin(field_type)
+        if base_type is list:
+            type_args = get_args(field_type)
+            if len(type_args) != 1:
+                raise ValueError(f'Field {field} must declare the type of the '
+                                 'list element.')
+            nested_type = type_args[0]
 
-        # Get ABIType from metadata
-        abi_type = None
-        for md in metadata:
-            if isinstance(md, ABIType):
-                abi_type = md
-                break
-        if abi_type is None:
-            raise ValueError(f'Field {field} has no ABIType metadata.')
+            if (
+                isclass(nested_type) and
+                issubclass(nested_type, pydantic.BaseModel)
+            ):
+                nested_types = get_abi_types_from_model(nested_type)
+                types.append(f'({",".join(nested_types)})[]')
+            else:
+                types.append(_get_abi_for_type(field, nested_type))
 
-        types.append(abi_type.name)
+            continue
+
+        if isclass(field_type) and issubclass(field_type, pydantic.BaseModel):
+            nested_types = get_abi_types_from_model(field_type)
+            types.append(f'({",".join(nested_types)})')
+            continue
+
+        types.append(_get_abi_for_type(field, field_type))
 
     return types
+
+
+def _get_resolved_values(value):
+    """Return the value or, if the value is a pydantic model, a tuple
+    containing the values
+
+    Parameters
+    ----------
+    value : any
+        Value to resolve
+
+    Returns
+    -------
+    any
+        Resolved value
+    """
+    if isinstance(value, pydantic.BaseModel):
+        return _get_values_from_model(value)
+    return value
+
+
+def _get_values_from_model(obj: pydantic.BaseModel) -> list:
+    """Return a list of values from the model
+
+    Parameters
+    ----------
+    obj : pydantic.BaseModel
+        Pydantic model with ABIType annotations
+
+    Returns
+    -------
+    list
+        List of values from the model
+    """
+    fields = obj.__fields__.keys()
+    data = []
+    for field in fields:
+        value = getattr(obj, field)
+
+        if isinstance(value, list):
+            data.append([_get_resolved_values(item) for item in value])
+        else:
+            data.append(_get_resolved_values(value))
+    return data
 
 
 def encode_model(obj: pydantic.BaseModel, packed: bool = False) -> bytes:
@@ -128,14 +203,65 @@ def encode_model(obj: pydantic.BaseModel, packed: bool = False) -> bytes:
     else:
         encode = eth_abi.encode
 
-    fields = obj.__fields__.keys()
-    data = [getattr(obj, x) for x in fields]
+    data = _get_values_from_model(obj)
     types = get_abi_types_from_model(obj)
 
     return encode(types, data)
 
 
 M = TypeVar('M', bound=pydantic.BaseModel)
+
+
+def _parse_to_model(model: M, data: list[any]) -> M:
+    """Parse a list of values into a model
+
+    Parameters
+    ----------
+    model : pydantic.BaseModel
+        Pydantic model with ABIType annotations
+    data : list[any]
+        List of values to parse
+
+    Returns
+    -------
+    pydantic.BaseModel
+        Parsed model
+    """
+    fields = model.__fields__.keys()
+    hints = get_type_hints(model, include_extras=True)
+
+    # Substitute values with parsed models if needed
+    for idx, field in enumerate(fields):
+        field_type = hints[field]
+
+        base_type = get_origin(field_type)
+        if base_type is list:
+            type_args = get_args(field_type)
+            if len(type_args) != 1:
+                raise ValueError(f'Field {field} must declare the type of the '
+                                 'list element.')
+            nested_type = type_args[0]
+
+            if (
+                isclass(nested_type) and
+                issubclass(nested_type, pydantic.BaseModel)
+            ):
+                new_data = [
+                    _parse_to_model(nested_type, x) for x in data[idx]
+                ]
+                if isinstance(data, tuple):
+                    data = list(data)
+                data[idx] = new_data
+
+        elif (
+            isclass(field_type) and
+            issubclass(field_type, pydantic.BaseModel)
+        ):
+            if isinstance(data, tuple):
+                data = list(data)
+            data[idx] = _parse_to_model(field_type, data[idx])
+
+    return model.parse_obj(dict(zip(fields, data)))
 
 
 def decode_to_model(data: bytes, model: M, packed: bool = False) -> M:
@@ -160,9 +286,7 @@ def decode_to_model(data: bytes, model: M, packed: bool = False) -> M:
     else:
         decode = eth_abi.decode
 
-    fields = model.__fields__.keys()
     types = get_abi_types_from_model(model)
     decoded = decode(types, data)
 
-    kwargs = dict(zip(fields, decoded))
-    return model.parse_obj(kwargs)
+    return _parse_to_model(model, decoded)
