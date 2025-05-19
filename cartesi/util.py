13ad6c0eaa7d291e5f781a70294eaff0bcff2b0e
Joao Garcia <jpmldg@gmail.com>
2025-05-19 19:33:22 +0100

    Added OO approach to create outputs

---
+++ cartesi/util.py
@@ -0,0 +1,68 @@
+# Copyright 2022 Cartesi Pte. Ltd.
+#
+# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from eth_utils import function_signature_to_4byte_selector
+from eth_abi import decode, encode
+from typing import List, Tuple, Any
+
+
+def hex_to_str(hex):
+    """Decode a hex string prefixed with "0x" into a UTF-8 string"""
+    return bytes.fromhex(hex[2:]).decode("utf-8")
+
+
+def str_to_hex(str):
+    """Encode a string as a hex string, adding the "0x" prefix"""
+    return "0x" + str.encode("utf-8").hex()
+
+def decode_payload(types: List[str], payload: str, ) -> Tuple[Any, ...]:
+    """
+    Decodes an ABI-encoded payload.
+
+    Args:
+        types (List[str]): A list of ABI types as strings.
+        payload (str): The hex-encoded payload to decode.
+
+    Returns:
+        Tuple[Any, ...]: A tuple containing the decoded values.
+    """
+    # Ensure the payload is in bytes format
+    payload_bytes = bytes.fromhex(payload[2:])  # Remove '0x' prefix and convert to bytes
+    return decode(types, payload_bytes)
+
+def decode_id_val(payload: str) -> Tuple[Any, ...]:
+    return decode(['uint256[]', 'uint256[]'], payload)
+
+
+def encode_function_call(function_signature: str, types: List[str], values: List) -> str:
+    """
+    Encodes a function call with the given signature and parameters.
+
+    Args:
+        function_signature (str): The signature of the function (e.g., 'transfer(address,uint256)').
+        types (List[str]): A list of ABI types as strings.
+        values (List): A list of values corresponding to the types.
+
+    Returns:
+        str: The hex-encoded function call data.
+    """
+    # Get the 4-byte selector from the function signature
+    selector = function_signature_to_4byte_selector(function_signature)
+    
+    # Encode the values based on the provided types
+    encoded_params = encode(types, values)
+    
+    # Concatenate the selector and the encoded parameters
+    return "0x" + (selector + encoded_params).hex()
+
+def encode_values(types: List[str], values: List):
+    return encode(types, values)
