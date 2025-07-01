efd043c2bcc61dcfd966968047c83511de4dd985
Lyno Ferraz <lynoFerraz@gmail.com>
2025-07-01 10:34:08 -0300

    fix: utils imports

--- cartesi/util.py
+++ cartesi/util.py
@@ -10,8 +10,8 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-from eth_utils import function_signature_to_4byte_selector
-from eth_abi import decode, encode
+from Crypto.Hash import keccak
+from eth_abi_lite import decode_abi, encode_abi
 from typing import List, Tuple, Any
 
 
@@ -37,10 +37,10 @@ def decode_payload(types: List[str], payload: str, ) -> Tuple[Any, ...]:
     """
     # Ensure the payload is in bytes format
     payload_bytes = bytes.fromhex(payload[2:])  # Remove '0x' prefix and convert to bytes
-    return decode(types, payload_bytes)
+    return decode_abi(types, payload_bytes)
 
 def decode_id_val(payload: str) -> Tuple[Any, ...]:
-    return decode(['uint256[]', 'uint256[]'], payload)
+    return decode_abi(['uint256[]', 'uint256[]'], payload)
 
 
 def encode_function_call(function_signature: str, types: List[str], values: List) -> str:
@@ -56,13 +56,16 @@ def encode_function_call(function_signature: str, types: List[str], values: List
         str: The hex-encoded function call data.
     """
     # Get the 4-byte selector from the function signature
-    selector = function_signature_to_4byte_selector(function_signature)
-    
+    sig_hash = keccak.new(digest_bits=256)
+    sig_hash.update(function_signature.encode('utf-8'))
+
+    selector = sig_hash.digest()[:4].hex()
+
     # Encode the values based on the provided types
-    encoded_params = encode(types, values)
-    
+    encoded_params = encode_abi(types, values)
+
     # Concatenate the selector and the encoded parameters
     return "0x" + (selector + encoded_params).hex()
 
 def encode_values(types: List[str], values: List):
-    return encode(types, values)
+    return encode_abi(types, values)
